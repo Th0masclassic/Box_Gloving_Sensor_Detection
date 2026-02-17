0886ab6ea9f21f14f857ba98dc5aaf5a19e9449f
unknown <bertinatemp@gmail.com>
2026-02-17 16:07:43 +0000

    Added Acceloremeter Driver .c and .h with the GPIO input

---
+++ Firmware/src/drivers/accelerometer_driver.c
@@ -0,0 +1,14 @@
+#include "accelerometer_driver.h"
+
+
+esp_err_t accel_init(){
+
+    gpio_config_t accelpin;
+    accelpin.pin_bit_mask = ACCEL_INIT_PIN;
+    accelpin.mode = GPIO_MODE_OUTPUT;
+    accelpin.pull_up_en = GPIO_PULLUP_DISABLE;
+    accelpin.pull_down_en = GPIO_PULLDOWN_DISABLE;
+
+    return gpio_config(&accelpin);
+
+}
